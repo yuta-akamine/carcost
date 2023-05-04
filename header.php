ee7e306eaab73c439181369aa3f93aa5686c23bf
yuta-akamine <happykaizokuou@gmail.com>
2023-05-04 20:04:49 +0900

    ファビコン追加

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap5.0を適用 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/dentaku.png">
   </head>
 
   <body>
