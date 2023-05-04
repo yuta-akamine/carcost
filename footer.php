28579ee05465f823aa29a4a7f3039470f378daa4
yuta-akamine <happykaizokuou@gmail.com>
2023-05-04 21:33:56 +0900

    グリッドシステム適用

--- footer.php
+++ footer.php
@@ -1,20 +1,32 @@
         <br>
-        <p>車選びの方はこちらから</p>
-        <a href="https://ok.goo-net.com">グーネット沖縄<br></a>
-        <a href="https://www.o-cross.net/">クロスロード<br></a>
-        <a href="https://www.carsensor.net/usedcar/okinawa/">カーセンサー<br></a>
-        
-        <p>日用品の購入はこちらから</p>
-        <a href="https://www.rakuten.co.jp/">楽天市場<br></a>
-        <a href="https://www.amazon.co.jp/">Amazon<br></a>
-        <a href=""><br></a>
+        <div class="row">
+
+          <div class="col-md-6">
+
+            <p>車選びの方はこちらから</p>
+            <a href="https://ok.goo-net.com">グーネット沖縄<br></a>
+            <a href="https://www.o-cross.net/">クロスロード<br></a>
+            <a href="https://www.carsensor.net/usedcar/okinawa/">カーセンサー<br></a>
+
+          </div>
+          
+          <div class="col-md-6">
+
+            <p>日用品の購入はこちらから</p>
+            <a href="https://www.rakuten.co.jp/">楽天市場<br></a>
+            <a href="https://www.amazon.co.jp/">Amazon<br></a>
+            <a href="https://www.monotaro.com/">モノタロウ<br></a>
+
+          </div>
+
+        </div> <!-- class="row" -->
 
       </main>
       
       <hr>
       
       <footer>
-        <p>※計算結果や確認した情報を参考にして発生するいかなる問題に対しても、当サイトは一切責任を負いません。<br>
+        <p>※計算結果や確認した情報を元に発生するいかなる問題に対しても、当サイトは一切責任を負いません。<br>
           ご理解いただいたうえで参考程度にご利用いただきますようお願いいたします。</p>
         <p>Copyright &copy; 2023 Yuta Akamine. All Rights Resarved.</p>
         <!-- <a href="">お問い合わせ・ご要望はこちら</a> -->
