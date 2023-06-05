3575974f5703584f67064e3bf4b9d36eb797e840
yuta-akamine <happykaizokuou@gmail.com>
2023-06-05 22:27:59 +0900

    🎨 自動返信メール テスト版コーディング

--- thanks.php
+++ thanks.php
@@ -22,13 +22,25 @@ echo '<h2>ただいま障害発生により正常に登録を完了すること�
 echo '<p>ご迷惑をおかけして大変申し訳ございません。<br>復旧までしばらくお待ちください。<br></p>';
 echo '<p>復旧後に再度ユーザー登録をお願いいたします。</p>';
 
-// サーバー接続問題なしの場合
+
+// サーバー接続問題なしの場合(本番環境でテスト必要)
+
+// デコード表示テスト用
+// html_entity_decode($username, ENT_QUOTES, "UTF-8");
+// echo $username . '<br>';
+
 // echo '<h2>' . $username . '様ありがとうございます。ユーザー登録が無事完了しました。</h2>';
 // echo $email . 'にメールをお送りしましたのでご確認ください。<br>';
-
+// $mail_sub = 'ユーザー登録を受け付けました。'; //メールタイトル
+// $mail_body = $username . "様\nユーザー登録ありがとうございます。"; //送信するメール本文の内容。改行のためダブルクォーテーション
+// $mail_body = html_entity_decode( $mail_body, ENT_QUOTES, "UTF-8"); //名前をデコードし代入
+// $mail_head = 'From:******@gmail.com'; //Fromの後は自動メール送信者である自分のアドレスを記載
+// mb_language('Japanese');
+// mb_internal_encoding("UTF-8");
+// mb_send_mail( $email, $mail_sub, $mail_body, $mail_head); //メールを送信する命令
 
 ?>
-<p class="space"><br></p>
+<p class="space"></p>
 <a href="index.php">
   <button type="button" class="btn btn-outline-dark">トップページへ</button>
 </a>
