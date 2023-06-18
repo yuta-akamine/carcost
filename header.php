1400426af94dcfa2299e10f3110ea49153bb7a7a
yuta-akamine <happykaizokuou@gmail.com>
2023-06-18 22:54:42 +0900

    🎨 fontawesomeアイコン追加

--- header.php
+++ header.php
@@ -9,6 +9,8 @@
     <title>コスパ計算</title>
     <!-- Bootstrap5.0を適用 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<!-- Webアイコン(Font Awesome)読込み -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="images/dentaku.png">
   </head>
@@ -18,14 +20,14 @@
     <div class ="container">
 
       <header>
-        <!-- <a href="index.php">
-          <img src="images/dentaku.png" alt="">
-        </a> -->
+        <a href="index.php" class="iconlink">
+          <i class="fa-solid fa-calculator fa-2x rightspace"></i>
+        </a>
         <h1>コスパ計算サイト</h1>
       </header>
     
       <hr>
-      
+
       <div class="wrapper">
 
         <main>
