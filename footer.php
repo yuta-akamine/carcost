9e88bd13f594ebf0a71acf18a10ccba8828abb45
yuta-akamine <happykaizokuou@gmail.com>
2023-06-19 21:31:47 +0900

    💄 余白の追加

--- footer.php
+++ footer.php
@@ -7,14 +7,14 @@
           <div class="row topspace">
 
             <div class="col-md-6">
-              <p>車選びの方はこちらから</p>
+              <p class="topspace">車選びの方はこちらから</p>
               <a href="https://ok.goo-net.com">グーネット沖縄<br></a>
               <a href="https://www.o-cross.net/">クロスロード<br></a>
               <a href="https://www.carsensor.net/usedcar/okinawa/">カーセンサー<br></a>
             </div>
             
             <div class="col-md-6">
-              <p>日用品の購入はこちらから</p>
+              <p class="topspace">日用品の購入はこちらから</p>
               <a href="https://www.rakuten.co.jp/">楽天市場<br></a>
               <a href="https://www.amazon.co.jp/">Amazon<br></a>
               <a href="https://www.monotaro.com/">モノタロウ<br></a>
