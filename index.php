a5c66d161d98ecb931ee0af36bb4182d0f533ad2
yuta-akamine <happykaizokuou@gmail.com>
2023-06-26 19:46:00 +0900

    ♻️ JavaScript別ファイルへ記述

--- index.php
+++ index.php
@@ -153,41 +153,43 @@ include(dirname ( __FILE__ ) . '/header.php'); //ヘッダーを読み込み
   )=計算結果へ
 
   <!-- 商品1と商品2の単位を統一 -->
-  <script>
-    function syncSelection1_1() {
-      const select1_1 = document.getElementById("select1_1");
-      const select1_2 = document.getElementById("select1_2");
-      select1_2.value = select1_1.value;
-    }
+  <script src="syncSelection.js">
+    // 'use strict';
 
-    function syncSelection1_2() {
-      const select1_1 = document.getElementById("select1_1");
-      const select1_2 = document.getElementById("select1_2");
-      select1_1.value = select1_2.value;
-    }
+    // function syncSelection1_1() {
+    //   const select1_1 = document.getElementById("select1_1");
+    //   const select1_2 = document.getElementById("select1_2");
+    //   select1_2.value = select1_1.value;
+    // }
 
-    function syncSelection2_1() {
-      const select2_1 = document.getElementById("select2_1");
-      const select2_2 = document.getElementById("select2_2");
-      select2_2.value = select2_1.value;
-    }
+    // function syncSelection1_2() {
+    //   const select1_1 = document.getElementById("select1_1");
+    //   const select1_2 = document.getElementById("select1_2");
+    //   select1_1.value = select1_2.value;
+    // }
 
-    function syncSelection2_2() {
-      const select2_1 = document.getElementById("select2_1");
-      const select2_2 = document.getElementById("select2_2");
-      select2_1.value = select2_2.value;
-    }
-    function syncSelection3_1() {
-      const select3_1 = document.getElementById("select3_1");
-      const select3_2 = document.getElementById("select3_2");
-      select3_2.value = select3_1.value;
-    }
+    // function syncSelection2_1() {
+    //   const select2_1 = document.getElementById("select2_1");
+    //   const select2_2 = document.getElementById("select2_2");
+    //   select2_2.value = select2_1.value;
+    // }
 
-    function syncSelection3_2() {
-      const select3_1 = document.getElementById("select3_1");
-      const select3_2 = document.getElementById("select3_2");
-      select3_1.value = select3_2.value;
-    }
+    // function syncSelection2_2() {
+    //   const select2_1 = document.getElementById("select2_1");
+    //   const select2_2 = document.getElementById("select2_2");
+    //   select2_1.value = select2_2.value;
+    // }
+    // function syncSelection3_1() {
+    //   const select3_1 = document.getElementById("select3_1");
+    //   const select3_2 = document.getElementById("select3_2");
+    //   select3_2.value = select3_1.value;
+    // }
+
+    // function syncSelection3_2() {
+    //   const select3_1 = document.getElementById("select3_1");
+    //   const select3_2 = document.getElementById("select3_2");
+    //   select3_1.value = select3_2.value;
+    // }
   </script>
 
   <!-- エスケープ処理テスト用
---
+++ syncSelection.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function syncSelection1_1() {
+  const select1_1 = document.getElementById("select1_1");
+  const select1_2 = document.getElementById("select1_2");
+  select1_2.value = select1_1.value;
+}
+
+function syncSelection1_2() {
+  const select1_1 = document.getElementById("select1_1");
+  const select1_2 = document.getElementById("select1_2");
+  select1_1.value = select1_2.value;
+}
+
+function syncSelection2_1() {
+  const select2_1 = document.getElementById("select2_1");
+  const select2_2 = document.getElementById("select2_2");
+  select2_2.value = select2_1.value;
+}
+
+function syncSelection2_2() {
+  const select2_1 = document.getElementById("select2_1");
+  const select2_2 = document.getElementById("select2_2");
+  select2_1.value = select2_2.value;
+}
+function syncSelection3_1() {
+  const select3_1 = document.getElementById("select3_1");
+  const select3_2 = document.getElementById("select3_2");
+  select3_2.value = select3_1.value;
+}
+
+function syncSelection3_2() {
+  const select3_1 = document.getElementById("select3_1");
+  const select3_2 = document.getElementById("select3_2");
+  select3_1.value = select3_2.value;
+}
